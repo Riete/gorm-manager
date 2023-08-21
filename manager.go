f57e1bdc046c248107c29606988d7c8a3c3789ce
riet <loli_520@126.com>
2023-08-21 17:35:47 +0800

    update

--- manager.go
+++ manager.go
@@ -12,11 +12,11 @@ type GormManager[T any] struct {
 	db    *gorm.DB
 }
 
-func (g *GormManager[T]) Session(opts ...ManagerOption) *gorm.DB {
+func (g *GormManager[T]) Session(opts ...Option) *gorm.DB {
 	return g.SessionContext(context.Background(), opts...)
 }
 
-func (g *GormManager[T]) SessionContext(ctx context.Context, opts ...ManagerOption) *gorm.DB {
+func (g *GormManager[T]) SessionContext(ctx context.Context, opts ...Option) *gorm.DB {
 	db := g.db.WithContext(ctx).Order("")
 	for _, opt := range opts {
 		opt(db)
@@ -24,11 +24,11 @@ func (g *GormManager[T]) SessionContext(ctx context.Context, opts ...ManagerOpti
 	return db
 }
 
-func (g *GormManager[T]) First(opts ...ManagerOption) (*T, error) {
+func (g *GormManager[T]) First(opts ...Option) (*T, error) {
 	return g.FirstContext(context.Background())
 }
 
-func (g *GormManager[T]) FirstContext(ctx context.Context, opts ...ManagerOption) (*T, error) {
+func (g *GormManager[T]) FirstContext(ctx context.Context, opts ...Option) (*T, error) {
 	model := new(T)
 	return model, g.SessionContext(ctx, opts...).First(model).Error
 }
@@ -49,77 +49,77 @@ func (g *GormManager[T]) BatchCreateContext(ctx context.Context, models ...*T) (
 	return models, g.SessionContext(ctx).Create(models).Error
 }
 
-func (g *GormManager[T]) FirstOrCreate(model *T, opts ...ManagerOption) (*T, error) {
+func (g *GormManager[T]) FirstOrCreate(model *T, opts ...Option) (*T, error) {
 	return g.FirstOrCreateContext(context.Background(), model, opts...)
 }
 
-func (g *GormManager[T]) FirstOrCreateContext(ctx context.Context, model *T, opts ...ManagerOption) (*T, error) {
+func (g *GormManager[T]) FirstOrCreateContext(ctx context.Context, model *T, opts ...Option) (*T, error) {
 	return model, g.SessionContext(ctx, opts...).FirstOrCreate(model).Error
 }
 
-func (g *GormManager[T]) Update(column string, value any, opts ...ManagerOption) (int64, error) {
+func (g *GormManager[T]) Update(column string, value any, opts ...Option) (int64, error) {
 	return g.UpdateContext(context.Background(), column, value, opts...)
 }
 
-func (g *GormManager[T]) UpdateContext(ctx context.Context, column string, value any, opts ...ManagerOption) (int64, error) {
+func (g *GormManager[T]) UpdateContext(ctx context.Context, column string, value any, opts ...Option) (int64, error) {
 	model := new(T)
 	result := g.SessionContext(ctx, opts...).Model(model).Update(column, value)
 	return result.RowsAffected, result.Error
 }
 
-func (g *GormManager[T]) Updates(update T, opts ...ManagerOption) (int64, error) {
+func (g *GormManager[T]) Updates(update T, opts ...Option) (int64, error) {
 	return g.UpdatesContext(context.Background(), update, opts...)
 }
 
-func (g *GormManager[T]) UpdatesContext(ctx context.Context, update T, opts ...ManagerOption) (int64, error) {
+func (g *GormManager[T]) UpdatesContext(ctx context.Context, update T, opts ...Option) (int64, error) {
 	model := new(T)
 	result := g.SessionContext(ctx, opts...).Model(model).Updates(update)
 	return result.RowsAffected, result.Error
 }
 
-func (g *GormManager[T]) UpdateColumn(column string, value any, opts ...ManagerOption) (int64, error) {
+func (g *GormManager[T]) UpdateColumn(column string, value any, opts ...Option) (int64, error) {
 	return g.UpdateColumnContext(context.Background(), column, value, opts...)
 }
 
-func (g *GormManager[T]) UpdateColumnContext(ctx context.Context, column string, value any, opts ...ManagerOption) (int64, error) {
+func (g *GormManager[T]) UpdateColumnContext(ctx context.Context, column string, value any, opts ...Option) (int64, error) {
 	model := new(T)
 	result := g.SessionContext(ctx, opts...).Model(model).UpdateColumn(column, value)
 	return result.RowsAffected, result.Error
 }
 
-func (g *GormManager[T]) UpdateColumns(update T, opts ...ManagerOption) (int64, error) {
+func (g *GormManager[T]) UpdateColumns(update T, opts ...Option) (int64, error) {
 	return g.UpdateColumnsContext(context.Background(), update, opts...)
 }
 
-func (g *GormManager[T]) UpdateColumnsContext(ctx context.Context, update T, opts ...ManagerOption) (int64, error) {
+func (g *GormManager[T]) UpdateColumnsContext(ctx context.Context, update T, opts ...Option) (int64, error) {
 	model := new(T)
 	result := g.SessionContext(ctx, opts...).Model(model).UpdateColumns(update)
 	return result.RowsAffected, result.Error
 }
 
-func (g *GormManager[T]) Delete(opts ...ManagerOption) error {
+func (g *GormManager[T]) Delete(opts ...Option) error {
 	return g.DeleteContext(context.Background(), opts...)
 }
 
-func (g *GormManager[T]) DeleteContext(ctx context.Context, opts ...ManagerOption) error {
+func (g *GormManager[T]) DeleteContext(ctx context.Context, opts ...Option) error {
 	model := new(T)
 	return g.SessionContext(ctx, opts...).Delete(model).Error
 }
 
-func (g *GormManager[T]) Find(opts ...ManagerOption) ([]T, error) {
+func (g *GormManager[T]) Find(opts ...Option) ([]T, error) {
 	return g.FindContext(context.Background(), opts...)
 }
 
-func (g *GormManager[T]) FindContext(ctx context.Context, opts ...ManagerOption) ([]T, error) {
+func (g *GormManager[T]) FindContext(ctx context.Context, opts ...Option) ([]T, error) {
 	var models []T
 	return models, g.SessionContext(ctx, opts...).Find(&models).Error
 }
 
-func (g *GormManager[T]) Count(opts ...ManagerOption) (int64, error) {
+func (g *GormManager[T]) Count(opts ...Option) (int64, error) {
 	return g.CountContext(context.Background(), opts...)
 }
 
-func (g *GormManager[T]) CountContext(ctx context.Context, opts ...ManagerOption) (int64, error) {
+func (g *GormManager[T]) CountContext(ctx context.Context, opts ...Option) (int64, error) {
 	var count int64
 	model := new(T)
 	return count, g.SessionContext(ctx, opts...).Model(model).Count(&count).Error
@@ -133,19 +133,19 @@ func (g *GormManager[T]) TransactionContext(ctx context.Context, f func(tx *gorm
 	return g.SessionContext(ctx).Transaction(f)
 }
 
-func (g *GormManager[T]) Scan(dst any, opts ...ManagerOption) error {
+func (g *GormManager[T]) Scan(dst any, opts ...Option) error {
 	return g.ScanContext(context.Background(), dst, opts...)
 }
 
-func (g *GormManager[T]) ScanContext(ctx context.Context, dst any, opts ...ManagerOption) error {
+func (g *GormManager[T]) ScanContext(ctx context.Context, dst any, opts ...Option) error {
 	return g.SessionContext(ctx, opts...).Scan(dst).Error
 }
 
-func (g *GormManager[T]) Rows(opts ...ManagerOption) (*sql.Rows, error) {
+func (g *GormManager[T]) Rows(opts ...Option) (*sql.Rows, error) {
 	return g.RowsContext(context.Background(), opts...)
 }
 
-func (g *GormManager[T]) RowsContext(ctx context.Context, opts ...ManagerOption) (*sql.Rows, error) {
+func (g *GormManager[T]) RowsContext(ctx context.Context, opts ...Option) (*sql.Rows, error) {
 	return g.SessionContext(ctx, opts...).Rows()
 }
 
@@ -157,51 +157,51 @@ func (g *GormManager[T]) ExecContext(ctx context.Context, sql string, args ...an
 	return g.SessionContext(ctx).Exec(sql, args...).Error
 }
 
-func (g *GormManager[T]) AssociationFind(column string, dst any, opts ...ManagerOption) error {
+func (g *GormManager[T]) AssociationFind(column string, dst any, opts ...Option) error {
 	return g.AssociationFindContext(context.Background(), column, dst, opts...)
 }
 
-func (g *GormManager[T]) AssociationFindContext(ctx context.Context, column string, dst any, opts ...ManagerOption) error {
+func (g *GormManager[T]) AssociationFindContext(ctx context.Context, column string, dst any, opts ...Option) error {
 	return g.SessionContext(ctx, opts...).Association(column).Find(dst)
 }
 
-func (g *GormManager[T]) AssociationAppend(column string, dst []any, opts ...ManagerOption) error {
+func (g *GormManager[T]) AssociationAppend(column string, dst []any, opts ...Option) error {
 	return g.AssociationAppendContext(context.Background(), column, dst, opts...)
 }
 
-func (g *GormManager[T]) AssociationAppendContext(ctx context.Context, column string, dst []any, opts ...ManagerOption) error {
+func (g *GormManager[T]) AssociationAppendContext(ctx context.Context, column string, dst []any, opts ...Option) error {
 	return g.SessionContext(ctx, opts...).Association(column).Append(dst...)
 }
 
-func (g *GormManager[T]) AssociationReplace(column string, dst []any, opts ...ManagerOption) error {
+func (g *GormManager[T]) AssociationReplace(column string, dst []any, opts ...Option) error {
 	return g.AssociationReplaceContext(context.Background(), column, dst, opts...)
 }
 
-func (g *GormManager[T]) AssociationReplaceContext(ctx context.Context, column string, dst []any, opts ...ManagerOption) error {
+func (g *GormManager[T]) AssociationReplaceContext(ctx context.Context, column string, dst []any, opts ...Option) error {
 	return g.SessionContext(ctx, opts...).Association(column).Replace(dst...)
 }
 
-func (g *GormManager[T]) AssociationDelete(column string, dst []any, opts ...ManagerOption) error {
+func (g *GormManager[T]) AssociationDelete(column string, dst []any, opts ...Option) error {
 	return g.AssociationDeleteContext(context.Background(), column, dst, opts...)
 }
 
-func (g *GormManager[T]) AssociationDeleteContext(ctx context.Context, column string, dst []any, opts ...ManagerOption) error {
+func (g *GormManager[T]) AssociationDeleteContext(ctx context.Context, column string, dst []any, opts ...Option) error {
 	return g.SessionContext(ctx, opts...).Association(column).Delete(dst...)
 }
 
-func (g *GormManager[T]) AssociationClear(column string, opts ...ManagerOption) error {
+func (g *GormManager[T]) AssociationClear(column string, opts ...Option) error {
 	return g.AssociationClearContext(context.Background(), column, opts...)
 }
 
-func (g *GormManager[T]) AssociationClearContext(ctx context.Context, column string, opts ...ManagerOption) error {
+func (g *GormManager[T]) AssociationClearContext(ctx context.Context, column string, opts ...Option) error {
 	return g.SessionContext(ctx, opts...).Association(column).Clear()
 }
 
-func (g *GormManager[T]) AssociationCount(column string, opts ...ManagerOption) int64 {
+func (g *GormManager[T]) AssociationCount(column string, opts ...Option) int64 {
 	return g.AssociationCountContext(context.Background(), column, opts...)
 }
 
-func (g *GormManager[T]) AssociationCountContext(ctx context.Context, column string, opts ...ManagerOption) int64 {
+func (g *GormManager[T]) AssociationCountContext(ctx context.Context, column string, opts ...Option) int64 {
 	return g.SessionContext(ctx, opts...).Association(column).Count()
 }
 
--- option.go
+++ option.go
@@ -4,57 +4,57 @@ import (
 	"gorm.io/gorm"
 )
 
-type ManagerOption func(db *gorm.DB)
+type Option func(db *gorm.DB)
 
-func WithConditions(query any, args ...any) ManagerOption {
+func WithConditions(query any, args ...any) Option {
 	return func(db *gorm.DB) {
 		db.Where(query, args...)
 	}
 }
 
-func WithNotConditions(query any, args ...any) ManagerOption {
+func WithNotConditions(query any, args ...any) Option {
 	return func(db *gorm.DB) {
 		db.Not(query, args...)
 	}
 }
 
-func WithOrConditions(query any, args ...any) ManagerOption {
+func WithOrConditions(query any, args ...any) Option {
 	return func(db *gorm.DB) {
 		db.Or(query, args...)
 	}
 }
 
-func WithSelect(query any, args ...any) ManagerOption {
+func WithSelect(query any, args ...any) Option {
 	return func(db *gorm.DB) {
 		db.Select(query, args...)
 	}
 }
 
-func WithOmit(column ...string) ManagerOption {
+func WithOmit(column ...string) Option {
 	return func(db *gorm.DB) {
 		db.Omit(column...)
 	}
 }
 
-func WithOffset(offset int) ManagerOption {
+func WithOffset(offset int) Option {
 	return func(db *gorm.DB) {
 		db.Offset(offset)
 	}
 }
 
-func WithLimit(limit int) ManagerOption {
+func WithLimit(limit int) Option {
 	return func(db *gorm.DB) {
 		db.Limit(limit)
 	}
 }
 
-func WithOrder(order string) ManagerOption {
+func WithOrder(order string) Option {
 	return func(db *gorm.DB) {
 		db.Order(order)
 	}
 }
 
-func WithGroupByHaving(group string, having ...any) ManagerOption {
+func WithGroupByHaving(group string, having ...any) Option {
 	return func(db *gorm.DB) {
 		if len(having) > 0 {
 			db.Group(group).Having(having[0], having[1:]...)
@@ -64,31 +64,31 @@ func WithGroupByHaving(group string, having ...any) ManagerOption {
 	}
 }
 
-func WithDistinct(args ...any) ManagerOption {
+func WithDistinct(args ...any) Option {
 	return func(db *gorm.DB) {
 		db.Distinct(args...)
 	}
 }
 
-func WithPreload(query string, conds ...any) ManagerOption {
+func WithPreload(query string, conds ...any) Option {
 	return func(db *gorm.DB) {
 		db.Preload(query, conds...)
 	}
 }
 
-func WithAttrs(attrs ...any) ManagerOption {
+func WithAttrs(attrs ...any) Option {
 	return func(db *gorm.DB) {
 		db.Attrs(attrs...)
 	}
 }
 
-func WithAssign(attrs ...any) ManagerOption {
+func WithAssign(attrs ...any) Option {
 	return func(db *gorm.DB) {
 		db.Assign(attrs...)
 	}
 }
 
-func WithRawSql(sql string) ManagerOption {
+func WithRawSql(sql string) Option {
 	return func(db *gorm.DB) {
 		db.Raw(sql)
 	}
